756ce462740bc6a9bd975d1477bf85e246552e92
nezbednik <nezbednik@atlas.cz>
2023-03-10 17:03:03 +0100

    reduce dependencies

--- grab_pcp_file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-cd "$(dirname "$0")"
-./grab_pcp.sh 2>/dev/null >./pcp_data.json
--- router_decode.sh
+++ router_decode.sh
@@ -4,12 +4,9 @@
 # parameters:
 #  1. hex-encoded AES-256 key (length: 64)
 #  2. content parameter from response
-#      feel free to pass it directly from the JSON;
-#      the first sed takes care of the escaping slashes
 #  3. iv parameter from response
 
-# dependencies: openssl, xxd, sed, cut
-# sed, cut from busybox will work
+# dependencies: openssl, xxd, jq
 
-echo "$2" | sed "s.\\\\..g" | openssl enc -d -a -A | openssl aes-256-cbc -d -K "$1" -iv "$(echo "$3" | openssl enc -d -a -A | xxd -c 0 -p | cut -b 1-32)"
+echo "$2" | openssl enc -d -a -A | openssl enc -aes-256-cbc -d -K "$1" -iv "$(jq --null-input --raw-output -M --arg s "$(echo "$3" | openssl enc -d -a -A | xxd -c 0 -p)" '$s[0:32]')"
 echo
