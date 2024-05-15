a6206a9b5d8162404a221dd21a86740540f78b5c
nezbednik <nezbednik@atlas.cz>
2024-05-15 19:41:23 +0200

    Add check for blank IP

--- grab_pcp_external.sh
+++ grab_pcp_external.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-./grab_pcp.sh | jq -M --raw-output --arg "port" "$1" ".Object[] | select(.RequiredInternalPort | tostring | startswith(\$port)) | .ExternalIPAddress" | sed "s.:.\\n.g" | tail -n 1
+export "NEWIP=$(./grab_pcp.sh | jq -M --raw-output --arg "port" "$1" ".Object | sort_by(.RequiredInternalPort) | .[] | select(.RequiredInternalPort | tostring | startswith(\$port)) | .ExternalIPAddress | split(\":\") | last" | tail -n1)"
+if ! [ "$NEWIP" = "0.0.0.0" ]; then
+	echo "$NEWIP"
+else
+	exit 1
+fi
